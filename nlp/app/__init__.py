4f4b9850a1c7f278a3a37e17eee1f7f0ea4e45aa
Gavin_Galusha <gavingalusha@gmail.com>
2024-05-20 22:02:52 -0700

    heroku

--- nlp/app/__init__.py
+++ nlp/app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 import os
+import spacy
 from .. import nlp_path
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'  # for CSRF
+
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 from . import routes
